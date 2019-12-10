e12d7d9f08498a5e934331cd385b0c00e5ef4573
gabin warnier de wailly <gabin.warnier-de-wailly@epitech.eu>
2019-12-10 15:34:35 +0100

    select music

--- app/src/main/java/com/example/sleewell/ui/sounds/SoundsFragment.kt
+++ app/src/main/java/com/example/sleewell/ui/sounds/SoundsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.sleewell.ui.sounds
 
+import android.app.PendingIntent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -13,19 +15,43 @@ import com.example.sleewell.R
 class SoundsFragment : Fragment() {
 
     private lateinit var soundsViewModel: SoundsViewModel
+    private  lateinit var listView: ListView
+    private lateinit var list: MutableList<String>
+    private lateinit var adapter: ListAdapter
+    private var mediaPlayer: MediaPlayer? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        soundsViewModel =
-            ViewModelProviders.of(this).get(SoundsViewModel::class.java)
+        soundsViewModel = ViewModelProviders.of(this).get(SoundsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_sounds, container, false)
-        val textView: TextView = root.findViewById(R.id.text_sounds)
-        soundsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        listView = root.findViewById(R.id.music_list_view) as ListView
+        list = ArrayList()
+
+        val fields = R.raw::class.java!!.fields
+        for (i in fields.indices) {
+            list.add(fields[i].name)
+        }
+        adapter = ArrayAdapter(root.context, android.R.layout.simple_list_item_1, list)
+        listView.adapter = adapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener{adapterView, view, i, l ->
+            if (mediaPlayer != null) {
+                mediaPlayer!!.release()
+            }
+            val singh = resources.getIdentifier(list[i], "raw", root.context.packageName)
+            mediaPlayer = MediaPlayer.create(root.context, singh)
+            mediaPlayer!!.start()
+        }
         return root
+    }
+
+    override fun onDestroy() {
+        mediaPlayer?.stop()
+        super.onDestroy()
     }
 }
