63d68285aaca9e9314a9c99ef41f49638b714704
Jostyck9 <hugo.berthome@epitech.eu>
2019-12-10 14:41:17 +0100

    adding npd, wifi and bluetooth functions

--- app/src/main/java/com/example/sleewell/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sleewell/ui/home/HomeFragment.kt
@@ -1,5 +1,10 @@
 package com.example.sleewell.ui.home
 
+import android.app.NotificationManager
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context.*
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +13,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.sleewell.R
+import androidx.preference.PreferenceManager
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.sleewell.SettingsActivity
+
 
 class HomeFragment : Fragment() {
 
@@ -21,11 +31,49 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        val root = inflater.inflate(com.example.sleewell.R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(com.example.sleewell.R.id.text_home)
+
+        // How to get preferences
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        if (prefs.all["SleepMode"] == true) {
+            textView.text = "Sleep ON"
+        } else {
+            textView.text = "Sleep OFF"
+        }
         return root
+    }
+
+    fun enableWifi(value: Boolean) {
+        val wifiManager : WifiManager = context?.getSystemService(WIFI_SERVICE) as WifiManager
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
+            wifiManager.setWifiEnabled(value)
+    }
+
+    private fun enableBluetouth(value: Boolean) {
+        val manager = BluetoothAdapter.getDefaultAdapter()
+
+        if (!value && manager.isEnabled) {
+            manager.disable()
+        } else if (value && !manager.isEnabled) {
+            manager.enable()
+        }
+    }
+
+    private fun enableDnd(value: Boolean) {
+        val notificationManager : NotificationManager = context?.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        // Check if the notification policy access has been granted for the app.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return
+        if (!notificationManager.isNotificationPolicyAccessGranted) {
+            val intent = Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+            startActivity(intent)
+        }
+        if (value) {
+            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE)
+        } else {
+            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL)
+        }
     }
 }
