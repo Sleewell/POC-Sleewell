64ae38d9946c5226ed011de97dc8fbf6db195fc6
gabin warnier de wailly <gabin.warnier-de-wailly@epitech.eu>
2020-02-16 18:18:13 +0100

    +add new background to the home fragment

--- app/src/main/java/com/example/sleewell/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sleewell/ui/home/HomeFragment.kt
@@ -1,23 +1,17 @@
 package com.example.sleewell.ui.home
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.preference.PreferenceManager
-import com.example.sleewell.ProtoActivated
 import com.example.sleewell.R
 import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_home.view.*
-
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -33,38 +27,15 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
         // How to get preferences
-        setParam(root)
         val customAnalogClock = root.findViewById(R.id.analog_clock) as CustomAnalogClock
         customAnalogClock.init(context)
         customAnalogClock.setAutoUpdate(true)
+
+        val ani = root.background as AnimationDrawable
+        ani.setEnterFadeDuration(10)
+        ani.setExitFadeDuration(5000)
+        ani.start()
+
         return root
-    }
-
-    fun setParam(root : View) {
-        val textView: TextView = root.findViewById(R.id.text_home)
-        val textHalo: TextView = root.findViewById(R.id.halo_home)
-        val textMusic: TextView = root.findViewById(R.id.music_home)
-        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-
-        if (prefs.all["SleepMode"] == true) {
-            textView.text = "Sleep ON"
-        } else {
-            textView.text = "Sleep OFF"
-        }
-        if (prefs.all["Halo"] == true) {
-            textHalo.text = "Halo ON"
-        } else {
-            textHalo.text = "Halo OFF"
-        }
-        if (prefs.all["Music"] == true) {
-            textMusic.text = "Music ON"
-        } else {
-            textMusic.text = "Music OFF"
-        }
-    }
-
-    override fun onResume() {
-        setParam(view!!)
-        super.onResume()
     }
 }
